45f186288d45577616ecc108b2c95a2f2b904f15
baconYao <bacon735392@gmail.com>
2019-03-11 20:33:09 +0800

    Add Ninjia-level2 exe2 and 3

---
+++ Ninjia-level2/exe3/exercise-3.go
@@ -0,0 +1,16 @@
+// Create TYPED and UNTYPED constants. Print the values of the constants
+
+package main
+
+import (
+	"fmt"
+)
+
+const (
+	a     = 42
+	b int = 43
+)
+
+func main() {
+	fmt.Println(a, b)
+}
