1234a07258bdad2c56d1e2a248e8760fbc4c8ef0
baconYao <bacon735392@gmail.com>
2019-03-19 21:04:21 +0800

    Add exercise-6

---
+++ Ninja-level3/exe6/exercise-6.go
@@ -0,0 +1,15 @@
+// Create a program that shows the “if statement” in action.
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	x := "Bacon Yao"
+
+	if x == "Bacon Yao" {
+		fmt.Println(x)
+	}
+}
